bdb3b84177de2ee2c6950e15a37320fa18765896
emanueldias01 <emanueldias01dev@gmail.com>
2024-09-23 16:03:01 -0300

    adicao dos arquivos

---
+++ docker-manager/src/test/java/com/emanueldias/docker_manager/DockerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanueldias.docker_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
